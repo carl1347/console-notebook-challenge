efe2033305c7065ce9bfd1efe914f1cd8ff5eab6
carl1347 <carlosotalor132005@gmail.com>
2025-08-04 17:43:16 -0500

    metodos de la clase Notebook

--- src/notebook.py
+++ src/notebook.py
@@ -8,7 +8,7 @@ class Note:
      LOW: str = "LOW"
 
      def __innit__(self, code: str, title: str, text: str, importance: str, creation_date: datetime):
-        self.code: str = code
+        self.code: int = code
         self.title: str = title
         self.text: str = text
         self.importance: str = importance
@@ -27,9 +27,13 @@ class Notebook:
     def __innit__(self, notes: list[Note]):
         self.notes: list[Note] = []
 
-
-
-
-
-
-
+    def add_note(self, title: str, text: str, importance: str)-> int:
+       pass
+    def delete_note(self, code: int):
+        pass
+    def important_notes()-> list[Note]
+        pass
+    def notes_by_tags(self, tags: str) -> list[Note]:
+        pass
+    def tag_with_most_notes(self) -> str:
+        pass
